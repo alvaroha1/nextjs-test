86a4cfbe8b8ed244741ecb759812d1f719027692
Alvaro Hernandez <alvarohernandezassens@gmail.com>
2021-12-11 23:38:20 +0100

    add basic styles navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,35 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import styles from "../styles/Layout.module.css";
+import { useRouter } from "next/router";
+interface Route {
+  name: string;
+  path: string;
+}
 
 function Layout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const routes: Route[] = [{name: "Home", path: "/"}, {name: "Rick and Morty", path: "/rickandmorty"}, {name: "Table", path: "/table"}, {name: "Ticktacktoe", path: "/ticktacktoe"}, {name: "Timer", path: "/timer"}]
   return (
-    <div>
+    <div className={styles.container}>
       <nav>
-        <Link href={`/`}>
-          <a>
-            <span>Home</span>
-          </a>
-        </Link>
-        <Link href={`/rickandmorty`}>
-          <a>
-            <span>Rick and Morty</span>
-          </a>
-        </Link>
-        <Link href={`/table`}>
-          <a>
-            <span>Table</span>
-          </a>
-        </Link>
-        <Link href={`/ticktacktoe`}>
-          <a>
-            <span>Ticktacktoe</span>
-          </a>
-        </Link>
-        <Link href={`/timer`}>
-          <a>
-            <span>Timer</span>
-          </a>
-        </Link>
+        {routes.map(route => {
+          return (<Link href={route.path} key={route.path}>
+            {router.route === route.path ? <span className={styles.item__selected}>{route.name}</span> : <span className={styles.item}>{route.name}</span>}
+          </Link>)
+        })}
       </nav>
       {children}
     </div>
