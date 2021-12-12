c491b56eb5bd4da5b5f332807a5eefc8777a30c2
Alvaro Hernandez <alvarohernandezassens@gmail.com>
2021-12-12 10:51:09 +0100

    wip table styles

--- pages/table.tsx
+++ pages/table.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import SortableTable from "../components/SortableTable"
 
 const TablePage: NextPage = () => {
   return (
@@ -7,6 +8,7 @@ const TablePage: NextPage = () => {
 
       <main className={styles.main}>
         {<p>Table Page</p>}
+        <SortableTable />
       </main>
 
       <footer className={styles.footer}>{ <p>footer</p> }</footer>
